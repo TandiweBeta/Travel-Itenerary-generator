bb18e4007cc2783279e9ad5951e2f50ce91861db
TandiweBeta <tandiwebeta@gmail.com>
2024-05-27 07:38:05 +0200

    Added the javascript structure to the project and and also added typewriter in javascript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+function generateItinerary(event){
+  event.preventDefault();
+  
+  let itineraryElement = document.querySelector("#itinerary");
+  itineraryElement.innerHTML = "Day 1, Day 2, Day 3";
+  new Typewriter("#itinerary", {
+    strings: "Day 1, Day 2, Day 3",
+    autoStart: true,
+    delay:1,
+    cursor:null,
+  });
+ 
+}
 
+let itineraryFormElement = document.querySelector("#itinerary-generator-form");
+itineraryFormElement.addEventListener("submit",generateItinerary);
